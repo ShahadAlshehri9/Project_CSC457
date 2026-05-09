17e93a6d48de4e18174e6ae3fc099bd5aa9cbc7d
Shahad Altulyan <shahadabdallaht0@gmail.com>
2026-05-09 17:56:41 +0300

    Update details.php

--- details.php
+++ details.php
@@ -27,6 +27,7 @@ $landmarks = toList($place['landmarks']);
 <html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($place['name']) ?> - اكتشف السعودية</title>
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
