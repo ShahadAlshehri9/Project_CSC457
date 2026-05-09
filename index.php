07340965880ab6769f96aa9f7d3cbb4e1ae59339
Shahad Altulyan <shahadabdallaht0@gmail.com>
2026-05-09 18:18:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>اكتشف السعودية - الرئيسية</title>
+    <script>
+    if (localStorage.getItem("nightMode") === "1") {
+        document.documentElement.classList.add("night");
+    }
+     </script>
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
 </head>
