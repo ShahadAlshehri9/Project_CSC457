0d5e663fd1f334d7dfaea0039dd59b563d22223d
Shahad Altulyan <shahadabdallaht0@gmail.com>
2026-05-09 17:56:13 +0300

    index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>اكتشف السعودية - الرئيسية</title>
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
